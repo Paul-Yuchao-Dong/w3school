86373f9b5496c7e62efe08525f7c48dbdd0517b8
Paul-Yuchao-Dong <Paulcynic@gmail.com>
2015-10-22 17:58:55 +0800

    change settings.py to start up the pipeline
    not sure what the 300 means

--- w3school/settings.py
+++ w3school/settings.py
@@ -61,9 +61,9 @@ NEWSPIDER_MODULE = 'w3school.spiders'
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'w3school.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'w3school.pipelines.W3SchoolPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
